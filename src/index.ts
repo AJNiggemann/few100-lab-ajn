2f841abfe1be87ff588e99efb9e7895524f1c30e
AJNiggemann <ajmci89@yahoo.com>
2019-03-08 20:11:11 +0000

    In-flight

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
 import './styles.css';
-console.log('Ready to Party');
+
+const tipSelected = document.querySelectorAll('.btn');
+tipSelected.forEach(ts => ts.addEventListener('click', processClick));
+
+let selectedTipAmt: number = 0;
+const enteredBillAmount: HTMLInputElement = <HTMLInputElement>document.getElementById('bill_amount');
+
+function processClick() {
+    const div = this as HTMLDivElement;
+    if (div.id === 'percent_10') {
+        selectedTipAmt = .10
+    } else if (div.id === 'percent_15') {
+        selectedTipAmt = .15
+    } else {
+        selectedTipAmt = .20
+    }
+    calculateTip(selectedTipAmt);
+}
+function calculateTip(ta: number) {
+    const billAmount = parseFloat(enteredBillAmount.value);
+    const tipAmount = billAmount * ta;
+    const totalBill = billAmount + tipAmount;
+    console.log(billAmount);
+    console.log(tipAmount);
+    console.log(totalBill);
+}
