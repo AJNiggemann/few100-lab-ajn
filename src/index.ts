72b0995328f6b6779cdde0c1be430a8a27000650
AJNiggemann <ajmci89@yahoo.com>
2019-03-28 17:14:13 +0000

    Updates 3/28/19 @ 1115am MST

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,104 @@
 import './styles.css';
 
-const tipSelected = document.querySelectorAll('.btn');
-const enteredBillAmount: HTMLInputElement = <HTMLInputElement>document.getElementById('bill_amount');
-let errorBorder = document.getElementById('bill_amount');
-let selectedTipAmt: number = 0;
-let errorSection: HTMLInputElement = document.querySelector('.errorDisplay');
-
-tipSelected.forEach(ts => ts.classList.add('disabled'));
-let billAmount: number = 0;
-enteredBillAmount.addEventListener('keyup', validateCost);
+//Initialization of Value
+let enteredBillAmountElmnt: HTMLInputElement = <HTMLInputElement>document.getElementById('bill_amount');
+let errorSectionElmnt: HTMLInputElement = document.querySelector('.errorDisplay');
+let tippercent: HTMLDivElement = document.querySelector('.pickedPercent');
+let billamt: HTMLLIElement = document.querySelector('.billAmt');
+let disptip: HTMLLIElement = document.querySelector('.percentPicked');
+let tipamt: HTMLLIElement = document.querySelector('.tipAmt');
+let totalbill: HTMLLIElement = document.querySelector('.billTotal');
+let errorBorderElmnt = document.getElementById('bill_amount');
+let tipSelectedBtn = document.querySelectorAll('.btn');
+let selectedTipAmtVal: number = 0;
+let tipPercent: number = 0;
+let billAmountVal: number = 0;
+let tipAmount: number = 0;
+let totalBill: number = 0;
+//
+tipSelectedBtn.forEach(ts => ts.classList.add('disabled'));
+enteredBillAmountElmnt.addEventListener('keyup', validateCost);
 
 function validateCost() {
-    billAmount = parseFloat(enteredBillAmount.value);
-    if (isNaN(billAmount)) {
-        errorSection.innerHTML = 'Invalid, please re-enter the amount of the bill...';
-        errorBorder = document.getElementById('bill_amount');
-        errorBorder.classList.add('error-input-border');
-        errorBorder.click;
+    billAmountVal = parseFloat(enteredBillAmountElmnt.value);
+    if (isNaN(billAmountVal)) {
+        errorSectionElmnt.innerHTML = 'Invalid, please re-enter the amount of the bill...';
+        errorBorderElmnt = document.getElementById('bill_amount');
+        errorBorderElmnt.classList.add('error-input-border');
         clearValues();
-    } else if (billAmount <= 0) {
-        tipSelected.forEach(ts => ts.classList.remove('disabled'));
-        tipSelected.forEach(ts => ts.addEventListener('click', processClick));
-    } else if (billAmount > 0) {
-        errorSection.innerHTML = "";
-        if (selectedTipAmt > 0) {
-            errorBorder = document.getElementById('bill_amount');
-            errorBorder.classList.remove('error-input-border');
-            calculateTip(selectedTipAmt);
+    } else if (billAmountVal <= 0) {
+        errorSectionElmnt.innerHTML = "";
+        tipSelectedBtn.forEach(ts => ts.classList.remove('disabled'));
+        tipSelectedBtn.forEach(ts => ts.addEventListener('click', processClick));
+    } else if (billAmountVal > 0) {
+        errorSectionElmnt.innerHTML = "";
+        if (selectedTipAmtVal > 0) {
+            errorBorderElmnt = document.getElementById('bill_amount');
+            errorBorderElmnt.classList.remove('error-input-border');
+            calculateTip(selectedTipAmtVal);
         } else {
-            errorBorder = document.getElementById('bill_amount');
-            errorBorder.classList.remove('error-input-border');
-            tipSelected.forEach(ts => ts.classList.remove('disabled'));
-            tipSelected.forEach(ts => ts.addEventListener('click', processClick));
+            errorBorderElmnt = document.getElementById('bill_amount');
+            errorBorderElmnt.classList.remove('error-input-border');
+            tipSelectedBtn.forEach(ts => ts.classList.remove('disabled'));
+            tipSelectedBtn.forEach(ts => ts.addEventListener('click', processClick));
         }
     }
 }
 function processClick() {
-    const button = this as HTMLButtonElement;
-    errorBorder.classList.remove('error-input-border');
-    tipSelected.forEach(ts => ts.classList.remove('disabled'));
+    let button = this as HTMLButtonElement;
+    errorBorderElmnt.classList.remove('error-input-border');
+    tipSelectedBtn.forEach(ts => ts.classList.remove('disabled'));
     button.classList.add('disabled');
     if (button.id === 'percent_10') {
-        if (billAmount <= 0) {
+        if (billAmountVal <= 0) {
             zeroBill();
         } else {
-            selectedTipAmt = 10,
-                calculateTip(selectedTipAmt)
-        };
+            selectedTipAmtVal = 10;
+            calculateTip(selectedTipAmtVal);
+        }
     } else if (button.id === 'percent_15') {
-        if (billAmount <= 0) {
+        if (billAmountVal <= 0) {
             zeroBill();
         } else {
-            selectedTipAmt = 15,
-                calculateTip(selectedTipAmt)
-        };
+            selectedTipAmtVal = 15;
+            calculateTip(selectedTipAmtVal);
+        }
     } else {
-        if (billAmount <= 0) {
+        if (billAmountVal <= 0) {
             zeroBill();
         } else {
-            selectedTipAmt = 20,
-                calculateTip(selectedTipAmt)
-        };
+            selectedTipAmtVal = 20;
+            calculateTip(selectedTipAmtVal);
+        }
     }
 }
 function zeroBill() {
-    errorSection.innerHTML = 'Bill needs to be more than zero, please re-enter...';
-    errorBorder = document.getElementById('bill_amount');
-    errorBorder.classList.add('error-input-border');
+    errorSectionElmnt.innerHTML = 'Bill needs to be more than zero, please re-enter...';
+    errorBorderElmnt = document.getElementById('bill_amount');
+    errorBorderElmnt.classList.add('error-input-border');
     clearValues();
 }
 function calculateTip(ta: number) {
-    let tipPercent: number = ta / 100;
-    let billAmount: number = parseFloat(enteredBillAmount.value);
-    let tipAmount: number = billAmount * tipPercent;
-    let totalBill: number = billAmount + tipAmount;
-    displayValues(billAmount, tipAmount, totalBill, ta);
+    tipPercent = ta / 100;
+    billAmountVal = parseFloat(enteredBillAmountElmnt.value);
+    tipAmount = billAmountVal * tipPercent;
+    totalBill = billAmountVal + tipAmount;
+    displayValues();
 }
-function displayValues(ba: number, ta: number, tb: number, tp: number) {
-    let tippercent: HTMLDivElement = document.querySelector('.pickedPercent');
-    tippercent.innerHTML = `You are tipping ${tp}%`;
-    let billamt: HTMLLIElement = document.querySelector('.billAmt');
-    billamt.innerHTML = `Bill Amount $${ba.toFixed(2)}`;
-    let disptip: HTMLLIElement = document.querySelector('.percentPicked');
-    disptip.innerHTML = `Tip Percentage: ${tp}%`;
-    let tipamt: HTMLLIElement = document.querySelector('.tipAmt');
-    tipamt.innerHTML = `Amount of tip: $${ta.toFixed(2)}`;
-    let totalbill: HTMLLIElement = document.querySelector('.billTotal');
-    totalbill.innerHTML = `Total to be Paid: $${tb.toFixed(2)}`;
+function displayValues() {
+    tippercent.innerHTML = `You are tipping ${selectedTipAmtVal}%`;
+    billamt.innerHTML = `Bill Amount $${billAmountVal.toFixed(2)}`;
+    disptip.innerHTML = `Tip Percentage: ${selectedTipAmtVal}%`;
+    tipamt.innerHTML = `Amount of tip: $${tipAmount.toFixed(2)}`;
+    totalbill.innerHTML = `Total to be Paid: $${totalBill.toFixed(2)}`;
 }
 function clearValues() {
-    enteredBillAmount.value = "";
-    selectedTipAmt = 0;
-    tipSelected.forEach(ts => ts.classList.add('disabled'));
-    let tippercent: HTMLDivElement = document.querySelector('.pickedPercent');
+    enteredBillAmountElmnt.value = "";
+    selectedTipAmtVal = 0;
+    tipSelectedBtn.forEach(ts => ts.classList.add('disabled'));
     tippercent.innerHTML = `You are tipping `;
-    let billamt: HTMLLIElement = document.querySelector('.billAmt');
     billamt.innerHTML = `Bill Amount $`;
-    let disptip: HTMLLIElement = document.querySelector('.percentPicked');
     disptip.innerHTML = `Tip Percentage: %`;
-    let tipamt: HTMLLIElement = document.querySelector('.tipAmt');
     tipamt.innerHTML = `Amount of tip: $`;
-    let totalbill: HTMLLIElement = document.querySelector('.billTotal');
     totalbill.innerHTML = `Total to be Paid: $`;
 }
